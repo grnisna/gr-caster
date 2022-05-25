22a8e0f43a3aff1753e06b96977ce8d781b06ba8
nisanAhmed <mdnisanahmed63@gmail.com>
2022-05-25 17:27:44 +0600

    add item form deshgn deone

--- src/components/DashboardSection/AddNewItem.js
+++ src/components/DashboardSection/AddNewItem.js
@@ -1,10 +1,208 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddNewItem = () => {
+    const {register, formState:{errors}} = useForm();
+
     return (
         <>
-            
+
             <h1 className='text-center text-4xl text-primary font-extrabold bg-base-100 py-10'>Add New Item</h1>
+
+            <div class="card w-full bg-base-100 shadow-xl">
+
+                <div class="card-body lg:max-w-96 bg-slate-400 mx-auto">
+
+                    <form>
+                        {/* name */}
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Item Name</span>
+                            </label>
+                            <input 
+                            type="text" 
+                            placeholder="Type here" 
+                            class="input input-bordered w-full max-w-xs"
+                            {...register('name',{
+                                required:{
+                                    value:true,
+                                    message:'Require Item name'
+                                }
+                            })}
+                            />
+                            <label class="label">
+                               {errors.name?.type === 'required' && <span class="label-text-alt text-error">{errors.name.message} </span>} 
+
+                            </label>
+                        </div>
+
+                        {/* model */}
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Item Model name</span>
+
+                            </label>
+                            <input 
+                            type="text" 
+                            placeholder="Type here" 
+                            class="input input-bordered w-full max-w-xs" 
+                            {...register('model',{
+                                required:{
+                                    value:true,
+                                    message:'Require  model name'
+                                }
+                            })}
+                            />
+                            <label class="label">
+                               {errors.model?.type === 'required' && <span class="label-text-alt text-error">{errors.model.message} </span>} 
+
+                            </label>
+                            
+                        </div>
+
+                        {/* weight */}
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">What is your name?</span>
+
+                            </label>
+                            <input 
+                            type="text" 
+                            placeholder="Type here" 
+                            class="input input-bordered w-full max-w-xs" 
+                            {...register('weight',{
+                                required:{
+                                    value:true,
+                                    message:'Require item weight'
+                                }
+                            })}
+                            />
+                            <label class="label">
+                               {errors.weight?.type === 'required' && <span class="label-text-alt text-error">{errors.weight.message} </span>} 
+
+                            </label>
+                        </div>
+                        {/* price */}
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">What is your name?</span>
+
+                            </label>
+                            <input 
+                            type="number" 
+                            placeholder="Type here" 
+                            class="input input-bordered w-full max-w-xs" 
+                            {...register('price',{
+                                required:{
+                                    value:true,
+                                    message:'Require item price'
+                                }
+                            })}
+                            />
+                            <label class="label">
+                               {errors.price?.type === 'required' && <span class="label-text-alt text-error">{errors.price.message} </span>} 
+
+                            </label>
+                        </div>
+                        {/* available */}
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">What is your name?</span>
+
+                            </label>
+                            <input 
+                            type="number" 
+                            placeholder="Type here" 
+                            class="input input-bordered w-full max-w-xs" 
+                            {...register('weight',{
+                                required:{
+                                    value:true,
+                                    message:'Require item available'
+                                }
+                            })}
+                            />
+                            <label class="label">
+                               {errors.available?.type === 'required' && <span class="label-text-alt text-error">{errors.available.message} </span>} 
+
+                            </label>
+                        </div>
+                        {/* minbook */}
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">What is your name?</span>
+
+                            </label>
+                            <input 
+                            type="number" 
+                            placeholder="Type here" 
+                            class="input input-bordered w-full max-w-xs" 
+                            {...register('minbook',{
+                                required:{
+                                    value:true,
+                                    message:'Require item minbook'
+                                }
+                            })}
+                            />
+                            <label class="label">
+                               {errors.minbook?.type === 'required' && <span class="label-text-alt text-error">{errors.minbook.message} </span>} 
+
+                            </label>
+                        </div>
+                        {/* description */}
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">What is your name?</span>
+
+                            </label>
+                            <input 
+                            type="text" 
+                            placeholder="Type here" 
+                            class="input input-bordered w-full max-w-xs" 
+                            {...register('description',{
+                                required:{
+                                    value:true,
+                                    message:'Require item description'
+                                }
+                            })}
+                            />
+                            <label class="label">
+                               {errors.description?.type === 'required' && <span class="label-text-alt text-error">{errors.description.message} </span>} 
+
+                            </label>
+                        </div>
+                        {/* image */}
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">What is your name?</span>
+
+                            </label>
+                            <input 
+                            type="file" 
+                            placeholder="Type here" 
+                            class="input input-bordered w-full max-w-xs" 
+                            {...register('image',{
+                                required:{
+                                    value:true,
+                                    message:'Require item image'
+                                }
+                            })}
+                            />
+                            <label class="label">
+                               {errors.image?.type === 'required' && <span class="label-text-alt text-error">{errors.image.message} </span>} 
+
+                            </label>
+                        </div>
+
+                        {/* submit  */}
+                        <div class="form-control w-full max-w-xs">
+                            <input className='w-full btn btn-primary hover:btn-warning'  type="submit" value='ADD ITEM' />
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
 
         </>
     );
